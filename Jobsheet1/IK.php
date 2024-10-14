fb2e4ac8ad051c966a2bd61a34b6345b2e793a78
RevanoAugustofa <revanoa4945@gmail.com>
2024-10-14 10:49:32 +0700

    update

--- Jobsheet1/IK.php
+++ Jobsheet1/IK.php
@@ -1,16 +1,19 @@
 <?php 
-
+//Definisi kelas mahasiswa
 class Mahasiswa {
+    //Atribut atau Properti
     public $nama;
     public $nim;
     public $jurusan;
 
+    //constructor
     public function __construct($nama, $nim, $jurusan){
         $this->nama = $nama;
         $this->nim = $nim;
         $this->jurusan = $jurusan;
     }
 
+    //Method fungsi tampil data
     public function tampilkanData(){
          return "Hai Nama Saya : $this->nama.<br> Npm saya : $this->nim.<br> Jurusan : $this->jurusan ";
     }
@@ -26,7 +29,7 @@ class Mahasiswa {
     }
 }
 
-//Membuat objek
+//membuat objek dan menampilkannya
 echo "<u>Data Awal</u><br>";
 $tampil = new Mahasiswa ("Revano Augustofa",230102071,"Teknik Informatika");
 echo $tampil->tampilkanData();
