f5d92b8a38f4d1dcaee9bdb6a0712d5824aa7e4b
RevanoAugustofa <revanoa4945@gmail.com>
2024-10-15 10:30:56 +0700

    update

--- Jobsheet3/Tugas.php
+++ Jobsheet3/Tugas.php
@@ -3,16 +3,59 @@
 class Person{
     private $name;
 
+    public function __construct($name){
+        $this->name = $name;
+    }
+
+    public function getNama(){
+        return $this->name;
+    }
+
+    public function getRole(){
+        echo "";
+    }
+
 }
 
 
 
 class Dosen extends Person{
+    private $nidn;
 
+    public function __construct($nidn,$name){
+        parent::__construct($name);
+        $this->nidn=$nidn;
+    }
+
+    public function getNama(){
+        return $this->name;
+    }
+
+    public function getRole(){
+        echo "Dosen";
+    }
+
+    public function setNama($name){
+        $this->name=$name;
+    }
 }
 
 class Mahasiswa extends Person{
 
+    private $nim;
+
+    public function __construct($nim,$name){
+        parent::__construct($name);
+        $this->nim = $nim;
+    }
+
+    public function getNama(){
+        return $this->name;
+    }
+
+    public function getRole(){
+        echo "Mahasiswa";
+    }
 }
 
 abstract class jurnal{
